ad258a17799392295cb5424f103bdc5619a3a2a3
italy11 <visa188@163.com>
2014-09-05 10:36:35 +0800

    t

---
+++ src/shell/restart.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+PID=`ps -ef|grep 'com.caucho.boot.WatchdogManager'|grep -v perl|grep -v grep|awk '{print $2}'`
+if [ -n "${PID}" ]; then
+    kill -9 ${PID}
+fi
+sleep 2
+
+su -c "/home/server/resin_test/bin/resin.sh start" resin
+
